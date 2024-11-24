0287332bd5f215e95bd310a73da10391366c863d
Landon Phillippi <landonphillippi@Landons-MacBook-Air.local>
2024-11-23 20:24:39 -0600

    first commit

---
+++ app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Not Found</h1>
+            <p>No foodie-related items exist on this page!</p>
+        </main>
+    );
+}
